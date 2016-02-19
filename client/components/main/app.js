ecafa976cc58762e5e2810c54a64c720b7211913
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2016-02-19 11:36:45 +0900

    Fix table

--- client/components/main/app.js
+++ client/components/main/app.js
@@ -37,88 +37,90 @@ export default class App extends Component {
 
     return (
       <section className='section'>
-        <table className='container table'>
-          <thead>
-            <tr>
-              <td>部門</td>
-              <td colSpan='2'>作品</td>
-              <td colSpan='3'>制作者</td>
-            </tr>
-          </thead>
+        <div className='container is-fluid'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <td>部門</td>
+                <td colSpan='2'>作品</td>
+                <td colSpan='3'>制作者</td>
+              </tr>
+            </thead>
 
-          <tbody>
-            {flatten(sections.map(section => {
-              const sectionData = works.filter((work) =>
-                work.section === section.id
-              );
+            <tbody>
+              {flatten(sections.map(section => {
+                const sectionData = works.filter((work) =>
+                  work.section === section.id
+                );
 
-              const creatorTotal = sectionData.reduce((total, {creators}) => {
-                return total + creators.length;
-              }, 0);
+                const creatorTotal = sectionData.reduce((total, {creators}) => {
+                  return total + creators.length;
+                }, 0);
 
-              return sectionData.map((work, workIndex) => {
-                const {creators} = work;
-                const isSectionFirst = workIndex === 0;
+                return sectionData.map((work, workIndex) => {
+                  const {creators} = work;
+                  const isSectionFirst = workIndex === 0;
 
-                return creators.map((creator, creatorIndex) => {
-                  const isCreatorFirst = creatorIndex === 0;
+                  return creators.map((creator, creatorIndex) => {
+                    const isCreatorFirst = creatorIndex === 0;
 
-                  return (
-                    <tr key={creator._id}>
-                      {isSectionFirst && isCreatorFirst && (
-                        <td
-                          className='nowrap'
-                          rowSpan={creatorTotal}>
-                          {this.getSectionName(section.id)}
+                    return (
+                      <tr key={creator._id}>
+                        {isSectionFirst && isCreatorFirst && (
+                          <td
+                            className='nowrap'
+                            rowSpan={creatorTotal}>
+                            {this.getSectionName(section.id)}
+                          </td>
+                        )}
+                        {isCreatorFirst && [
+                          <td
+                            key={`${creator._id}-name`}
+                            className='nowrap'
+                            rowSpan={creators.length}>
+                            {work.name}
+                          </td>,
+                          <td
+                            key={`${creator._id}-description`}
+                            rowSpan={creators.length}>
+                            {work.description}
+                          </td>
+                        ]}
+                        <td className='nowrap'>{creator.name}</td>
+                        <td className='nowrap'>{creator.role}</td>
+                        <td className='nowrap'>
+                          {(() => {
+                            switch (creator.status) {
+                              case STANDING:
+                                return (
+                                  <button
+                                    className='button is-success'
+                                    type='button'
+                                    onClick={
+                                      this.handleClick.bind(
+                                        this, work._id, creator._id
+                                      )
+                                    }>
+                                    通知する
+                                  </button>
+                                );
+                              case TAKEN:
+                                return (
+                                  <button
+                                    className='button is-danger is-disabled'
+                                    type='button'>取込中</button>
+                                );
+                            }
+                          })()}
                         </td>
-                      )}
-                      {isCreatorFirst && [
-                        <td
-                          key={`${creator._id}-name`}
-                          className='nowrap'
-                          rowSpan={creators.length}>
-                          {work.name}
-                        </td>,
-                        <td
-                          key={`${creator._id}-description`}
-                          rowSpan={creators.length}>
-                          {work.description}
-                        </td>
-                      ]}
-                      <td className='nowrap'>{creator.name}</td>
-                      <td className='nowrap'>{creator.role}</td>
-                      <td className='nowrap'>
-                        {(() => {
-                          switch (creator.status) {
-                            case STANDING:
-                              return (
-                                <button
-                                  className='button is-success'
-                                  type='button'
-                                  onClick={
-                                    this.handleClick.bind(
-                                      this, work._id, creator._id
-                                    )
-                                  }>
-                                  通知する
-                                </button>
-                              );
-                            case TAKEN:
-                              return (
-                                <button
-                                  className='button is-danger is-disabled'
-                                  type='button'>取込中</button>
-                              );
-                          }
-                        })()}
-                      </td>
-                    </tr>
-                  );
+                      </tr>
+                    );
+                  });
                 });
-              });
-            }))}
-          </tbody>
-        </table>
+              }))}
+            </tbody>
+          </table>
+        </div>
       </section>
     );
   }
